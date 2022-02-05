84e885bebad37b22404d6b78fa111347a57a1ec8
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2022-02-05 18:51:45 +0100

    1.2

---
+++ src/main/java/fr/samlegamer/droptherock/DropTheRock.java
@@ -0,0 +1,52 @@
+package fr.samlegamer.droptherock;
+
+import fr.samlegamer.droptherock.item.ItemsMod;
+import fr.samlegamer.droptherock.item.RocksModRegistry;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(value = DropTheRock.MODID)
+@Mod.EventBusSubscriber(modid = DropTheRock.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DropTheRock
+{
+	public static final String MODID = "droptherock";
+	
+	public DropTheRock()
+	{
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::ClientSetup);
+		
+		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+		ItemsMod.ITEM.register(bus);	
+		RocksModRegistry.BLOCK.register(bus);
+		
+		if(ModList.get().isLoaded("byg"))
+		{
+			RocksModRegistry.InitBYG();
+		}
+		
+		if(ModList.get().isLoaded("quark"))
+		{
+			RocksModRegistry.InitQuark();
+		}
+		
+		if(ModList.get().isLoaded("biomesoplenty"))
+		{
+			RocksModRegistry.InitBOP();
+		}
+	}
+	
+	private void setup(FMLCommonSetupEvent event)
+	{
+		
+	}
+	
+	private void ClientSetup(FMLClientSetupEvent event)
+	{
+		
+	}
+}
